f3c268333750cf509a98c25ccc5b9faba092a354
Esa Harju <liquiddragon@users.noreply.github.com>
2018-02-21 11:04:29 +0200

    Added log file functionality

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,19 @@
+import datetime
 import socket
+import time
 from flask import Flask
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
+    ts = time.time()
+    with open('/mnt/ceclivinontheedge/cec.log', mode='r') as outfile:
+        outfile.write(socket.gethostname())
+        outfile.write('\t')
+        outfile.write(datetime.datetime.fromtimestamp(ts).strftime('%d.%m.%Y %H:%M:%S'))
+        outfile.write('\n')
+
     return "Hello World! Greetings from "+socket.gethostname()+"\n"
 
 
